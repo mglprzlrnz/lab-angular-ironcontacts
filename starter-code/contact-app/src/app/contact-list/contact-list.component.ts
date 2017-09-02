44c8402bac3de75e072e0c6984af1be3187ac250
Tuyet-Trinh thi Vu <mglprzlrnz@gmail.com>
2017-09-02 16:42:22 +0200

    Iteration 1 & 2 completed

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -19,6 +19,8 @@ export class ContactListComponent implements OnInit {
   addContact(){
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.contacts.push(this.newContact);
+    this.newContact = {};
     // clear inputs
   }
 }
